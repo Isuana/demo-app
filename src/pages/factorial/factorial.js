a771a0bb553946d3c2bd9ebf435e3440f332c8b7
Klaudia <klaudiad18@outlook.com>
2022-11-20 22:13:50 +0100

    Add Factorial Page for Calculation

--- src/pages/factorial/factorial.js
+++ src/pages/factorial/factorial.js
@@ -2,17 +2,41 @@ import './factorial.css';
 
 function Factorial() {
 
+  const clickedCalculation = () => {
+    let numer = document.getElementById('number').value;
+    let result = 1;
+
+    if (numer === '' || isNaN(numer)) {
+      document.getElementById('result').innerHTML = 'To nie liczba';
+      return;
+    }
+
+    if (numer < 0) {
+      document.getElementById('result').innerHTML = 'Wartość nie może być poniżej zera';
+      return;
+    }
+
+    for (let i = 1; i <= numer; i++) {
+      result = result * i;
+    }
+
+    document.getElementById('result').innerHTML = result;
+    document.getElementById('number').value = '';
+  }
+
+  function clearResult() {
+    document.getElementById('result').innerHTML = '';
+  }
 
   return (
     <div className="Factorial">
       <h3>Liczenie silni</h3> 
-      <input type="text" />
+      <input type="text" id='number' onChange={() => clearResult()} />
       <br/>
-      <input type="button" value="Policz" />
+      <input className="calculation-button" type="button" value="Policz" onClick={() => clickedCalculation()} />
+      <div id='result'></div>
     </div>
   );
 }
-
-
 
 export default Factorial;
