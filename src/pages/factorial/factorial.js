ad1defabaa58cda27aeb59cf405a27ed522121de
Isuana <106450739+Isuana@users.noreply.github.com>
2022-11-21 23:32:28 +0100

    Add Factorial Page for Calculation (#1)
    * Add Factorial Page for Calculation 
    * Add tests

--- src/pages/factorial/factorial.js
+++ src/pages/factorial/factorial.js
@@ -2,17 +2,42 @@ import './factorial.css';
 
 function Factorial() {
 
+  const clickedCalculation = () => {
+    let numer = document.getElementById('number').value;
+    let result = 1;
+
+    if (numer === '' || isNaN(numer)) {
+      document.getElementById('result').innerHTML = 'To nie jest liczba!';
+      return;
+    }
+
+    if (numer <= 0) {
+      document.getElementById('result').innerHTML = 'Wartość nie może być poniżej jeden!';
+      return;
+    }
+
+    for (let i = 1; i <= numer; i++) {
+      result = result * i;
+    }
+
+    document.getElementById('result').innerHTML = result;
+    document.getElementById('number').value = '';
+  }
+
+  function clearResult() {
+    document.getElementById('result').innerHTML = '';
+  }
 
   return (
-    <div className="Factorial">
-      <h3>Liczenie silni</h3> 
-      <input type="text" />
-      <br/>
-      <input type="button" value="Policz" />
+    <div>
+      <h3 className='style-head'>Liczenie silni</h3>
+      <br />
+      <input type="text" id='number' onChange={() => clearResult()} />
+      <br />
+      <input type="button" value="Policz" className="calculation-button" onClick={() => clickedCalculation()} />
+      <div id='result' className='calculation-result'></div>
     </div>
   );
 }
-
-
 
 export default Factorial;
